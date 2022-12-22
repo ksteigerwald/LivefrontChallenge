736c4f6b531cd519787793a19b35bc3d2b43b379
Kris Steigerwald <kris.steigerwald@gmail.com>
2022-12-22 16:49:20 -0700

    stash commit to regen xcode proj

--- Package.swift
+++ Package.swift
@@ -12,16 +12,18 @@ let package = Package(
             targets: ["LivefrontChallenge"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.0")
+        .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.0"),
+        .package(url: "https://github.com/AliSoftware/OHHTTPStubs", from: "9.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LivefrontChallenge",
-            dependencies: []),
-        .testTarget(
-            name: "LivefrontChallengeTests",
-            dependencies: ["LivefrontChallenge"]),
+            dependencies: ["SwiftGenPlugin"]),
+        .testTarget(name: "LivefrontChallengeTests",
+        dependencies: [
+            "OHHTTPStubs"
+        ])
     ]
 )
