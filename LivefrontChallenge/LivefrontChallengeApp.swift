aa20a74a2f4b1e1925866c0edc0d9dcbe9b2fb3c
Kris Steigerwald <kris.steigerwald@gmail.com>
2022-12-22 12:13:49 -0700

    Initial Commit

---
+++ LivefrontChallenge/LivefrontChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivefrontChallengeApp.swift
+//  LivefrontChallenge
+//
+//  Created by Kris Steigerwald on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct LivefrontChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
