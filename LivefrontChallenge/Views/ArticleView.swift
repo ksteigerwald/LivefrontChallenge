dbecc14978ad5e6f734cf302a29a4cacbea72b4d
Kris Steigerwald <kris.steigerwald@gmail.com>
2022-12-25 13:05:49 -0700

    refactor news feed items

---
+++ LivefrontChallenge/Views/ArticleView.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleView.swift
+//  LivefrontChallenge
+//
+//  Created by Kris Steigerwald on 12/25/22.
+//
+
+import SwiftUI
+
+struct ArticleView: View {
+    @EnvironmentObject var app: AppEnvironment
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
--- LivefrontChallenge/Views/NewsFeed.swift
+++ LivefrontChallenge/Views/NewsFeed.swift
@@ -29,29 +29,7 @@ struct NewsFeed: View {
                 ScrollView {
                     VStack(alignment: .leading, spacing: 20) {
                         ForEach(app.articles.news, id: \.id) { article in
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text(article.categories)
-                                        .font(Font.DesignSystem.bodySmallMedium)
-                                        .foregroundColor(Color.DesignSystem.greyscale500)
-                                    Text(article.title)
-                                        .font(Font.DesignSystem.headingH6)
-                                        .foregroundColor(Color.DesignSystem.greyscale50)
-                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                                        .fixedSize(horizontal: false, vertical: true)
-                                        .lineLimit(2)
-                                }
-                                AsyncImage(url: URL(string: article.imageUrl)) { image in
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(maxWidth: .infinity, alignment: .trailing)
-
-                                } placeholder: {
-                                    Color.DesignSystem.greyscale50
-                                }
-                                .frame(width: 80, height: 80)
-                            }
+                            NewsItem(article: article)
                         }
                     }
                 }
