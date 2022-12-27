950f8a85c4d2bb4a197f984b731176326b47ed20
Kris Steigerwald <kris.steigerwald@gmail.com>
2022-12-27 14:14:16 -0700

    remove print statements

--- LivefrontChallenge/Utility/StringExtension.swift
+++ LivefrontChallenge/Utility/StringExtension.swift
@@ -12,11 +12,8 @@ extension String {
     /// - Returns: An array containing the headline and body, or `nil` if there are fewer than 2 lines in the string.
     func parseHeadlineAndBody() -> [String] {
         let filtered = (self as NSString).components(separatedBy: "\n").filter { element in
-            print(element)
             return element.trimmingCharacters(in: .whitespacesAndNewlines).count > 0
         }
-        print("******")
-        print(filtered)
         guard filtered.count >= 2 else { return ["Headline Error:", "Content Generation Error:"] }
         return filtered
     }
