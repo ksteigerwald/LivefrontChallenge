9c1cac3076cb9277d80f1c19f369b2d7c7ccac53
Kris Steigerwald <kris.steigerwald@gmail.com>
2023-01-01 13:03:28 -0700

    docC

--- LivefrontChallenge/Utility/PropertyWrappers/Feed.swift
+++ LivefrontChallenge/Utility/PropertyWrappers/Feed.swift
@@ -8,17 +8,30 @@
 import Foundation
 import SwiftUI
 import Combine
-
+/// A class for managing the feed of articles
+///
+/// This class handles retrieving articles from the `ArticleRepository` and managing their state.
 public class Feed: ObservableObject {
 
+    /// An array of articles contained in the feed
     public var list: [ArticleFeedItem] = []
 
+    /// Boolean value for tracking whether the feed items have been loaded
     @Published public var isFeedItemLoaded: Bool = false
 
+    /// The shared instance of the `Feed` class
     public static let shared = Feed()
+
+    /// The repository used to retrieve articles
     private let repository: ArticleRepository
+
+    /// A set of `AnyCancellable` to manage cancellable operations
     var cancellables = Set<AnyCancellable>()
 
+    /// Initializer for the `Feed` class
+    ///
+    /// - Parameters:
+    ///   - repository: The `ArticleRepository` used to retrieve articles.
     init(repository: ArticleRepository = ArticleRepository()) {
         self.repository = repository
     }
@@ -42,7 +55,13 @@ public class Feed: ObservableObject {
     }
 
 }
-
+/// A `FeedProperty` is a `DynamicProperty` that wraps a `Feed` object
+/// - Parameters:
+///   - feed: The `Feed` object to be wrapped. Defaults to `.shared`
+/// - Example Usage:
+///     ```
+///     @FeedProperty var feed: Feed = .shared
+///     ```
 @propertyWrapper
 public struct FeedProperty: DynamicProperty {
     @ObservedObject public var feed: Feed
