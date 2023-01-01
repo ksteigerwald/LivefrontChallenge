a3e9fb2c36399f56124947717440d7bb02e948a7
Kris Steigerwald <kris.steigerwald@gmail.com>
2023-01-01 13:31:17 -0700

    code cleanup

--- LivefrontChallenge/Repository/ArticleRepository.swift
+++ LivefrontChallenge/Repository/ArticleRepository.swift
@@ -16,10 +16,14 @@ protocol ArticleInterface {
     ///   - articles: The list of articles to summarize.
     func generateSummaryArticle(category: String, articles: [String]) -> Future<OpenAIResponse, Error>
 
-    /// Fetches latest news articles
-    func getLatestArticles(limit: Int) async -> AnyPublisher<Result<[ArticleFeedItem], Error>, Error>
+    /// Query for the latest articles from the News API.
+    ///  - parameter limit: the number of articles to fetch
+    ///  - returns: a `Future` containing a `CryptoCompareResponse` object or an `Error`
+    func queryForArticles(limit: Int) -> Future<CryptoCompareResponse, Error>
 
-    /// Generate a new article from a source and selected prompt
+    /// Generate an article from a prompt using OpenAI's API
+    ///  - parameter prompt: the prompt to generate content from
+    ///  - returns: a `Future` containing an `OpenAIResponse` object or an `Error`
     func generateArticleFromSource(prompt: Prompts) async -> Future<OpenAIResponse, Error>
 }
 
@@ -42,10 +46,6 @@ public class ArticleRepository: ObservableObject, ArticleInterface {
         self.newsService = newsService
     }
 
-    /// Generates a summary article for the given category and list of articles.
-    /// - Parameters:
-    ///   - category: The category of the articles.
-    ///   - articles: The list of articles to summarize.
     public func generateSummaryArticle(category: String, articles: [String]) -> Future<OpenAIResponse, Error> {
         let articles = articles.prefix(10).joined(separator: ",\n")
         let params = OpenAIRequestParams(
@@ -61,9 +61,7 @@ public class ArticleRepository: ObservableObject, ArticleInterface {
             try await self.service.getSummaries(requestParams: params).get()
         })
     }
-    /// Query for the latest articles from the News API.
-    ///  - parameter limit: the number of articles to fetch
-    ///  - returns: a `Future` containing a `CryptoCompareResponse` object or an `Error`
+
     func queryForArticles(limit: Int = 5) -> Future<CryptoCompareResponse, Error> {
         let params = CryptoCompareRequestParams()
         return Future(asyncFunc: {
@@ -71,31 +69,6 @@ public class ArticleRepository: ObservableObject, ArticleInterface {
         })
     }
 
-    /// Get the latest articles from the News API
-    /// - parameter limit: the number of articles to fetch
-    /// - returns: a publisher emitting a `Result` containing an array of `ArticleFeedItem` objects or an `Error`
-    func getLatestArticles(limit: Int = 5) -> AnyPublisher<Result<[ArticleFeedItem], Error>, Error> {
-        queryForArticles()
-            .map { Result.success($0.articles) }
-            .mapError { $0 as Error }
-            .eraseToAnyPublisher()
-    }
-
-    /// Get content from OpenAI's API
-    /// - parameter prompt: the prompt to generate content from
-    /// - returns: a publisher emitting a `Result` containing an `Article` object or an `Error`
-    func getAIContent(prompt: Prompts) -> AnyPublisher<Result<Article, Error>, Error> {
-        generateArticleFromSource(prompt: prompt)
-            .map { $0.choices[0] }
-            .map { Article(category: "Summary", document: $0.text)}
-            .map { Result.success($0)}
-            .mapError { $0 as Error }
-            .eraseToAnyPublisher()
-    }
-
-    /// Generate an article from a prompt using OpenAI's API
-    ///  - parameter prompt: the prompt to generate content from
-    ///  - returns: a `Future` containing an `OpenAIResponse` object or an `Error`
     func generateArticleFromSource(prompt: Prompts) -> Future<OpenAIResponse, Error> {
         let params = OpenAIRequestParams(
             model: Prompts.Models.davinci003.rawValue,
